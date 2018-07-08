053a658dba7da1610bd62a81da47ca932d504fdd
Adham Muhammad <admuaq@gmail.com>
2018-07-08 17:32:23 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array
+  instantiate_new_array = []
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["",""]
+end
+
+def first_element(taylor_swift)
+  taylor_swift[0]
+end
+  
